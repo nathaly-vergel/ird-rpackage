955546fef57331491f55351cdfaf4a3465aa670b
nathaly-vergel <n.vergel@campus.lmu.de>
2026-02-27 14:20:38 +0100

    Fixed ParamSet$subset usage (not in-place anymore, returns ParamSet)
    ParamSet$subset() does not modify the object in-place -> it returns a new ParamSet
    
    the previous code assumed in-place behavior, which caused generate_design_grid() to operate on the full parameter space and produce an enormous Cartesian product.
    
    ERROR: Error in CJ(sorted = sorted, unique = unique, c(19L, 19L, 19L, 19L, 19L, :
    The cross-product of the elements provided to CJ() would result in 81000000000 rows exceeding .Machine$integer.max == 2147483647
    Timing stopped at: 0.3 0.02 0.29
    
    Now the result of $subset() is explicitly reassigned to ps_sub to ensure a 1D ParamSet before generating the grid

--- irdpackage/R/helpers.R
+++ irdpackage/R/helpers.R
@@ -349,7 +349,7 @@ get_max_box = function (x_interest, fixed_features, predictor, param_set, desire
       return(c(val_name, val_name))
     }
     ps_sub = param_set$clone(deep = TRUE)
-    ps_sub$subset(i_name)
+    ps_sub = ps_sub$subset(i_name)
     grid1d = paradox::generate_design_grid(ps_sub, resolution = resolution)$data
     x_interest_sub = data.table::copy(x_interest)
     x_interest_sub[, (i_name):=NULL]
