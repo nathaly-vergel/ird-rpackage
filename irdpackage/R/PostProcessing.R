6398df852a2f9257800c538335d651ae17b16869
nathaly-vergel <n.vergel@campus.lmu.de>
2026-03-12 09:39:56 +0100

    Use assert_choice for strategy_ties
    Validate strategy_ties as a single allowed option instead of treating it as a names vector, because code expects it to be character(1)

--- irdpackage/R/PostProcessing.R
+++ irdpackage/R/PostProcessing.R
@@ -32,7 +32,7 @@ PostProcessing = R6::R6Class("PostProcessing", inherit = RegDescMethod,
       checkmate::qassert(subbox_relsize, "N?(0,1]")
       checkmate::assert_integerish(evaluation_n, lower = 0)
       checkmate::assert_numeric(paste_alpha, len = 1L)
-      checkmate::assert_names(strategy_ties, subset.of = c("preddist", "random"))
+      checkmate::assert_choice(strategy_ties, choices = c("preddist", "random"))
       # assign private attr
       private$subbox_relsize = subbox_relsize
       private$evaluation_n = evaluation_n
