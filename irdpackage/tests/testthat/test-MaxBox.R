bf6f2a8d1ddffc514c040a6a4fbe93d6689f5004
nathaly-vergel <n.vergel@campus.lmu.de>
2026-04-27 18:20:56 +0200

    Uncommented MaxBox test
    Test was previously commented out, because the handling of fixed_features was not supported. This has been fixed by last PR.

--- irdpackage/tests/testthat/test-MaxBox.R
+++ irdpackage/tests/testthat/test-MaxBox.R
@@ -1,21 +1,21 @@
-# test_that("Regression and mixed features + fixed_features", {
-#   set.seed(54542142)
-#   mydf = mtcars
-#   mydf$am = as.factor(mydf$am)
-#   mydf$vs = as.factor(mydf$vs)
-#   mydf$cyl = as.integer(mydf$cyl)
-#
-#   rf = randomForest(mpg ~ ., data = mydf, ntree = 5L)
-#   pred = iml::Predictor$new(rf, data = mydf, y = "mpg")
-#
-#   x_interest = head(subset(mydf, select = -mpg), 1)
-#   desired_range = c(15, 25)
-#   mairep = MaxBox$new(pred, quiet = TRUE)
-#   mair = mairep$find_box(x_interest = x_interest, desired_range = desired_range, fixed_features = c("carb"))
-#   expect_true(all.equal(mair$box$lower[["carb"]], x_interest$carb))
-#   expect_true(all.equal(mair$box$upper[["carb"]], x_interest$carb))
-# })
-#
+test_that("Regression and mixed features + fixed_features", {
+  set.seed(54542142)
+  mydf = mtcars
+  mydf$am = as.factor(mydf$am)
+  mydf$vs = as.factor(mydf$vs)
+  mydf$cyl = as.integer(mydf$cyl)
+
+  rf = randomForest(mpg ~ ., data = mydf, ntree = 5L)
+  pred = iml::Predictor$new(rf, data = mydf, y = "mpg")
+
+  x_interest = head(subset(mydf, select = -mpg), 1)
+  desired_range = c(15, 25)
+  mairep = MaxBox$new(pred, quiet = TRUE)
+  mair = mairep$find_box(x_interest = x_interest, desired_range = desired_range, fixed_features = c("carb"))
+  expect_true(all.equal(mair$box$lower[["carb"]], x_interest$carb))
+  expect_true(all.equal(mair$box$upper[["carb"]], x_interest$carb))
+})
+
 
 
 test_that("Box is covered by true box", {
